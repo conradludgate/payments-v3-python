551942f71963a89d697016d07f43c6b3ce3145f7
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-09-14 13:26:18 +0100

    move auth into api

--- src/sdk/api.py
+++ src/sdk/api.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 from typing import Any, Dict, Optional
 from uuid import uuid4
 import json
@@ -6,8 +7,36 @@ import json
 # third-party imports
 import requests
 from truelayer_signing import HttpMethod, sign_with_pem
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
 
-from auth import KID, PRIVATE_KEY, TL_BASE_URL, token
+KID = os.getenv("TL_KID")
+CLIENT_ID = os.getenv("TL_CLIENT_ID")
+CLIENT_SECRET = os.getenv("TL_CLIENT_SECRET")
+
+if not CLIENT_ID:
+    raise ValueError("TL_CLIENT_ID not in Environment Variables")
+if not CLIENT_SECRET:
+    raise ValueError("TL_CLIENT_SECRET not in Environment Variables")
+if not KID:
+    raise ValueError("TL_KID not in Environment Variables")
+
+with open("ec512-private.pem") as f:
+    PRIVATE_KEY = f.read()
+
+if not PRIVATE_KEY:
+    raise ValueError("ec512-private.pem file could not be found")
+
+# the base url to use
+TL_BASE_URL: str = "t7r.dev"
+
+client = BackendApplicationClient(client_id=CLIENT_ID)
+oauth = OAuth2Session(client=client)
+token = oauth.fetch_token(
+    token_url=f"https://auth.{TL_BASE_URL}/connect/token",
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET)
+
 
 def do_api_request(method: HttpMethod, path: str, json_body: Optional[Any], query: Optional[Dict[str, str]]):
     url = f"https://api.{TL_BASE_URL}{path}"
