c741931a1b5a77a6c892465e15c7ff50149ba471
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-09-15 00:30:10 +0100

    state

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 # std imports
-from ctypes import Union
 import os
 from datetime import datetime, timedelta
+from typing import Union
+import urllib
+import uuid
+from app.sdk.api import GH_CLIENT_ID, GH_CLIENT_SECRET
 
 from app.sdk.auth_flow import start_auth_flow
 from app.sdk.models import Currency, MandateType, PeriodAlignment
@@ -13,7 +16,7 @@ from truelayer_signing.errors import TlSigningException
 import json
 import requests
 import uvicorn
-from fastapi import FastAPI, Request, Header, Form
+from fastapi import FastAPI, Request, Header, Form, Response, Cookie
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
@@ -24,20 +27,81 @@ BASE_HOST = os.getenv("BASE_HOST")
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def root():
-    return templates.TemplateResponse("index.html")
+class User:
+    def __init__(self, name: str):
+        self.name = name
+        self.payments = []
+        self.mandate = None
 
-@app.get("/mandate/{mandate_id}", response_class=HTMLResponse)
-async def mandate(mandate_id: str):
-    return templates.TemplateResponse("mandate.html", {"mandate_id": mandate_id})
+github_users = {}
+users = {}
+
+@app.get("/", response_class=HTMLResponse)
+async def root(
+    request: Request,
+    id: Union[str, None] = Cookie(default=None)
+):
+    global users, payments
+    print(users)
+    user = None
+    balance = 100000
+    if id is not None:
+        user = users[id]
+        for payment in user.payments:
+            for event in payments[payment]:
+                if event['type'] == "payment_executed":
+                    balance -= 10000
+
+    return templates.TemplateResponse("index.html", {"request": request, 'user': user, 'balance': balance})
+
+@app.get("/login")
+async def login():
+    query = urllib.parse.urlencode({
+        'client_id': GH_CLIENT_ID, 
+        'redirect_uri': "https://vrp.conrad.cafe/login/github",
+        'state': 'vrp',
+        'allow_signup': False,
+    })
+    return RedirectResponse(f"https://github.com/login/oauth/authorize?{query}", status_code=303)
+
+@app.get("/login/github")
+async def auth(code: str, request: Request):
+    global users, github_users
+    data = {
+        'client_id': GH_CLIENT_ID, 
+        'client_secret': GH_CLIENT_SECRET, 
+        'redirect_uri': "https://vrp.conrad.cafe/login/github",
+        'code': code,
+    }
+    headers = { 'Accept': 'application/json' }
+    token = requests.post("https://github.com/login/oauth/access_token", headers=headers, json=data).json()
+    
+    headers = { 'Authorization': f'Bearer {token["access_token"]}' }
+    user = requests.get("https://api.github.com/user", headers=headers).json()
+
+    try:
+        id = github_users[user["login"]]
+    except:
+        id = str(uuid.uuid4())
+        github_users[user["login"]] = id
+        users[id] = User(user["name"])
+
+    resp = templates.TemplateResponse("redirect.html", {
+        "request": request,
+		"path": "/",
+		"text": "Complete Login",
+	})
+    resp.set_cookie(key="id", value=id)
+    return resp
 
 @app.get("/callback")
 async def callback(mandate_id: str):
-    return RedirectResponse(f"/mandate/{mandate_id}", status_code=303)
+    return RedirectResponse(f"/", status_code=303)
 
 @app.post("/mandate")
-async def post_mandate():
+async def post_mandate(id: str = Cookie(default=None)):
+    global users
+    user = users[id]
     mandate = create_mandate({
         'mandate': {
             'type': MandateType.sweeping,
@@ -57,42 +121,62 @@ async def post_mandate():
         },
         'currency': Currency.GBP,
         'user': {
-            "id": "df69744a-fe8f-4acc-8b89-ba863297234c",
-            "email": "conrad@lud.co",
-            "name": "Co lu",
+            "id": id,
+            "email": "user@example.com",
+            "name": user.name,
         },
         "constraints": {
             "valid_from": datetime.utcnow(),
             "valid_to": datetime.utcnow() + timedelta(days=1),
-            "maximum_individual_amount": 5000,
+            "maximum_individual_amount": 10000,
             "periodic_limits": {
                 "month": {
-                    "maximum_amount": 10000,
+                    "maximum_amount": 20000,
                     "period_alignment": PeriodAlignment.consent
                 }
             }
         }
     })
+    user.mandate = mandate["id"]
     resp = start_auth_flow(mandate["id"], {
         'redirect': {
             'return_uri': f"{BASE_HOST}/callback"
         }
     })
-    print(resp)
     return RedirectResponse(resp["authorization_flow"]["actions"]["next"]["uri"], status_code=303)
 
+payments = {}
+
 @app.post("/payment", response_class=HTMLResponse)
-async def post_payment(amount: int = Form(), mandate_id: str = Form()):
+async def post_payment(id: str = Cookie(default=None)):
+    global payments, users
+    user = users[id]
     payment = create_payment({
-			"amount_in_minor": amount,
+			"amount_in_minor": 10000,
 			"currency":      "GBP",
 			"payment_method": {
 				"type":      "mandate",
-				"mandate_id": mandate_id,
+				"mandate_id": user.mandate,
 			}
     })
-    print(payment)
-    return templates.TemplateResponse("payment.html", {"mandate_id": mandate_id})
+    payment_id = payment["id"]
+    user.payments.append(payment_id)
+    payments[payment_id] = []
+    return RedirectResponse(f"/payment/{payment_id}", status_code=303)
+
+@app.get("/payment/{payment_id}", response_class=HTMLResponse)
+async def payment(request: Request, payment_id: str):
+    global payments
+    data = {"request": request}
+    events = payments[payment_id]
+    if len(events) > 0:
+        event = events[len(events)-1]
+        if event["type"] == "payment_executed":
+            data["executed"] = True
+        elif event["type"] == "payment_failed":
+            data["failure_reason"] = event["failure_reason"]
+        
+    return templates.TemplateResponse("payment.html", data)
 
 @app.get("/teapot", status_code=418, response_class=HTMLResponse)
 async def teapot():
@@ -117,7 +201,8 @@ async def webhook(
     tl_signature: str = Header(default=None),
     x_tl_webhook_timestamp: str = Header(default=None)
 ):
-    print(await request.json())
+    global payments
+    event = await request.json()
 
     jws_header = extract_jws_header(tl_signature)
     jku = jws_header.jku
@@ -135,6 +220,12 @@ async def webhook(
         .add_headers({"x-tl-webhook-timestamp": x_tl_webhook_timestamp}) \
         .set_body((await request.body()).decode()) \
         .verify(tl_signature)
+
+    try:
+        payment_id = event['payment_id']
+        payments[payment_id].append(event)
+    except:
+        pass
 
 def start():
     uvicorn.run(app, port=3000, log_level="info", host="0.0.0.0", proxy_headers=True)
--- app/sdk/api.py
+++ app/sdk/api.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from lib2to3.pgen2 import token
 import os
 from typing import Any, Dict, Optional
 from uuid import uuid4
@@ -11,31 +12,42 @@ from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
 KID = os.getenv("TL_KID")
-CLIENT_ID = os.getenv("TL_CLIENT_ID")
-CLIENT_SECRET = os.getenv("TL_CLIENT_SECRET")
+TL_CLIENT_ID = os.getenv("TL_CLIENT_ID")
+TL_CLIENT_SECRET = os.getenv("TL_CLIENT_SECRET")
+GH_CLIENT_ID = os.getenv("GH_CLIENT_ID")
+GH_CLIENT_SECRET = os.getenv("GH_CLIENT_SECRET")
 PRIVATE_KEY = os.getenv("TL_PRIVATE_KEY")
 
-if not CLIENT_ID:
+if not TL_CLIENT_ID:
     raise ValueError("TL_CLIENT_ID not in Environment Variables")
-if not CLIENT_SECRET:
+if not TL_CLIENT_SECRET:
     raise ValueError("TL_CLIENT_SECRET not in Environment Variables")
+if not GH_CLIENT_ID:
+    raise ValueError("GH_CLIENT_ID not in Environment Variables")
+if not GH_CLIENT_SECRET:
+    raise ValueError("GH_CLIENT_SECRET not in Environment Variables")
 if not KID:
     raise ValueError("TL_KID not in Environment Variables")
 if not PRIVATE_KEY:
     raise ValueError("TL_PRIVATE_KEY not in Environment Variables")
 
-print(CLIENT_ID, CLIENT_SECRET)
-
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-}
+headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
 data = {
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
+    'client_id': TL_CLIENT_ID,
+    'client_secret': TL_CLIENT_SECRET,
     'grant_type': 'client_credentials',
     'scope': 'payments recurring_payments:sweeping'
 }
 token = requests.post("https://auth.t7r.dev/connect/token", headers=headers, data=data).json()
+token_time = datetime.now()
+
+def get_tl_token():
+    global token, token_time
+    if token_time + timedelta(seconds=int(token["expires_in"])) > datetime.now():
+        token = requests.post("https://auth.t7r.dev/connect/token", headers=headers, data=data).json()
+        token_time = datetime.now()
+    return token["access_token"]
+
 
 def do_api_request(method: HttpMethod, path: str, json_body: Optional[Any], query: Optional[Dict[str, str]]):
     url = f"https://api.t7r.dev{path}"
@@ -55,9 +67,8 @@ def do_api_request(method: HttpMethod, path: str, json_body: Optional[Any], quer
         .sign()
     )
 
-    access_token = token["access_token"]
     headers = {
-        "Authorization": f"Bearer {access_token}",
+        "Authorization": f"Bearer {get_tl_token()}",
         "Content-Type": "application/json",
         "Idempotency-Key": idempotency_key,
         "Tl-Signature": signature,
